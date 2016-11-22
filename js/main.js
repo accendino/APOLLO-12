bb77c04ade880ad49eacabd7dd19576baa193288
Olya Olya <achendina.com>
2016-11-22 17:38:51 +0000

    1. navivation section is typeset (modified: index.html, css/main.css, less/main.less, js/main.js; created: less/sections/_navigation.less; 2. sorted project images accordingly in sections (created: img/bg/, img/icons/, img/images/; 3. navigation-icon image import into the project (img/icons/navigation-icon.png)

--- js/main.js
+++ js/main.js
@@ -1,6 +1,32 @@
 $(document).ready(function() {
 
-	/* Call plugins hrere ...*/
+	// скрипт: показываем/скрываем выпадающее меню/под смартфоны
+	//это переменные под кнопку и меню
+        var pull = $('#navigation-toggle');
+        var menu = $('.navigation ul');
 
+        //menuheight = menu.height(); 
+        
+        //говорим, что будет происходить при нажатии кнопки
+        $(pull).on("click", function(e){
+            // отменяет стандартное поведение ссылки в браузере
+            e.preventDefault();
+            // открытие/скрытие меню
+            menu.slideToggle();
+
+            //добавляем модификатор --active
+            $(this).toggleClass('navigation__toggle-button--active');
+        });
+
+    // при увеличении размера окна -отображает скрытое меню БОЖЕМОЙ
+    $(window).resize(function(){
+        var w = $(window).width();
+        if(w > 640 && menu.is(':hidden')){
+            menu.removeAttr('style');
+        }
+    });
 	
 });
+
+  
+         
